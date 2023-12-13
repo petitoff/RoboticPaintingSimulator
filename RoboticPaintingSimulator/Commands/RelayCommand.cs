569ce0ee14e7e41be824c077a198644686dbbeff
Błażej Domagała <petitoff@outlook.com>
2023-12-13 01:17:29 +0100

    Update RelayCommand to allow nullable canExecute parameter

--- RoboticPaintingSimulator/Commands/RelayCommand.cs
+++ RoboticPaintingSimulator/Commands/RelayCommand.cs
@@ -6,15 +6,15 @@ namespace RoboticPaintingSimulator.Commands;
 public class RelayCommand : ICommand
 {
     private readonly Action<object?> _execute;
-    private readonly Predicate<object?> _canExecute;
-    
+    private readonly Predicate<object?>? _canExecute;
+
     public event EventHandler? CanExecuteChanged
     {
         add => CommandManager.RequerySuggested += value;
         remove => CommandManager.RequerySuggested -= value;
     }
 
-    public RelayCommand(Action<object?> execute, Predicate<object?> canExecute)
+    public RelayCommand(Action<object?> execute, Predicate<object?>? canExecute = null)
     {
         _execute = execute;
         _canExecute = canExecute;
@@ -22,7 +22,7 @@ public class RelayCommand : ICommand
 
     public bool CanExecute(object? parameter)
     {
-        return _canExecute(parameter);
+        return _canExecute == null || _canExecute(parameter);
     }
 
     public void Execute(object? parameter)
