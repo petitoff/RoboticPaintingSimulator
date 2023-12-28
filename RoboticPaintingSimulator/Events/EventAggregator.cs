cae47aacce90e1cf8be8d356dad6ee5fe7cc630f
Błażej Domagała <petitoff@outlook.com>
2023-12-28 12:36:30 +0100

    Add event subscription and publishing functionality to EventAggregator

--- RoboticPaintingSimulator/Events/EventAggregator.cs
+++ RoboticPaintingSimulator/Events/EventAggregator.cs
@@ -1,6 +1,40 @@
+using System;
+using System.Collections.Generic;
+using RoboticPaintingSimulator.ViewModels;
+
 namespace RoboticPaintingSimulator.Events;
 
 public class EventAggregator
 {
-    
+    private static EventAggregator _instance;
+    public static EventAggregator Instance => _instance ?? (_instance = new EventAggregator());
+
+    private readonly Dictionary<Type, List<Delegate>> _eventHandlers = new Dictionary<Type, List<Delegate>>();
+
+    public void Subscribe<T>(Action<T> handler)
+    {
+        if (_eventHandlers.ContainsKey(typeof(T)))
+        {
+            _eventHandlers[typeof(T)].Add(handler);
+        }
+        else
+        {
+            _eventHandlers[typeof(T)] = new List<Delegate> { handler };
+        }
+    }
+
+    public void Publish<T>(T message)
+    {
+        if (_eventHandlers.TryGetValue(typeof(T), out var handlers))
+        {
+            foreach (var handler in handlers)
+            {
+                if (handler is Action<T> action)
+                {
+                    action.Invoke(message);
+                }
+            }
+        }
+    }
 }
+
