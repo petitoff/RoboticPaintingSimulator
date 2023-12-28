d02930240c67becbd8724d3685ffd3f4fc27d47a
Błażej Domagała <petitoff@outlook.com>
2023-12-28 12:36:40 +0100

    Refactor Element class to implement INotifyPropertyChanged interface

--- RoboticPaintingSimulator/Models/Element.cs
+++ RoboticPaintingSimulator/Models/Element.cs
@@ -1,9 +1,44 @@
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
 namespace RoboticPaintingSimulator.Models;
 
-public class Element
+public class Element : INotifyPropertyChanged
 {
-    public int Id { get; set; }
-    public bool IsRedPainted { get; set; }
-    public bool IsBluePainted { get; set; }
-    public bool IsGreenPainted { get; set; }
+    private bool _isRedPainted;
+    private bool _isBluePainted;
+    private bool _isGreenPainted;
+    private string _status;
+
+    public bool IsRedPainted
+    {
+        get => _isRedPainted;
+        set { _isRedPainted = value; OnPropertyChanged(); }
+    }
+
+    public bool IsBluePainted
+    {
+        get => _isBluePainted;
+        set { _isBluePainted = value; OnPropertyChanged(); }
+    }
+
+    public bool IsGreenPainted
+    {
+        get => _isGreenPainted;
+        set { _isGreenPainted = value; OnPropertyChanged(); }
+    }
+
+    public string Status
+    {
+        get => _status;
+        set { _status = value; OnPropertyChanged(); }
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
 }
