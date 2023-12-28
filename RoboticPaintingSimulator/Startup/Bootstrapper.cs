670cb450fb9bd7014b4421e31297bee7a7e73e56
Błażej Domagała <petitoff@outlook.com>
2023-12-28 12:36:53 +0100

    Add view and view model registrations to Bootstrapper class

--- RoboticPaintingSimulator/Startup/Bootstrapper.cs
+++ RoboticPaintingSimulator/Startup/Bootstrapper.cs
@@ -1,6 +1,29 @@
+using Autofac;
+using RoboticPaintingSimulator.Converters;
+using RoboticPaintingSimulator.Events;
+using RoboticPaintingSimulator.ViewModels;
+using RoboticPaintingSimulator.Views;
+
 namespace RoboticPaintingSimulator.Startup;
 
 public class Bootstrapper
 {
-    
+    public IContainer Bootstrap()
+    {
+        var builder = new ContainerBuilder();
+
+        builder.RegisterType<ElementsView>().AsSelf();
+        builder.RegisterType<ElementsViewModel>().AsSelf();
+
+        builder.RegisterType<ConfigurationView>().AsSelf();
+        builder.RegisterType<ConfigurationViewModel>().SingleInstance();
+
+        builder.RegisterType<MainWindow>().AsSelf();
+        builder.RegisterType<MainViewModel>().AsSelf();
+        
+        builder.RegisterType<RobotsViewModel>().AsSelf();
+        builder.RegisterType<RobotsView>().AsSelf();
+
+        return builder.Build();
+    }
 }
