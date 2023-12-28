3aebc05bfaa660cebf958bf6c62da69bd15089ea
Błażej Domagała <petitoff@outlook.com>
2023-12-28 12:36:23 +0100

    Refactor App.xaml.cs to use Autofac for dependency injection

--- RoboticPaintingSimulator/App.xaml.cs
+++ RoboticPaintingSimulator/App.xaml.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
+using Autofac;
+using RoboticPaintingSimulator.Startup;
+using RoboticPaintingSimulator.Views;
 
 namespace RoboticPaintingSimulator
 {
@@ -13,5 +10,14 @@ namespace RoboticPaintingSimulator
     /// </summary>
     public partial class App : Application
     {
+        private void App_OnStartup(object sender, StartupEventArgs e)
+        {
+            var bootstrapper = new Bootstrapper();
+            var container = bootstrapper.Bootstrap();
+
+            var mainWindow = container.Resolve<MainWindow>();
+
+            mainWindow.Show();
+        }
     }
 }
